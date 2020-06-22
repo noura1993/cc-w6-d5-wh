4688a18524008745278ef44062fb94e52bbea615
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-22 01:45:51 +0100

    Refactor css and add done/undo button to set status of todos

--- js/app.js
+++ js/app.js
@@ -21,15 +21,34 @@ const createTodo = function (form) {
     todoItem.classList.add('todo-item');
 
     const todo = document.createElement('input');
-    todo.classList.add('todo-item-text');
+    todo.classList.add('notes');
     todo.readOnly = true;
     todo.value = `${form.title.value}:  ${form.notes.value}`;
     todoItem.appendChild(todo);
-  
+
+    const checkTodo = document.createElement('input');
+    checkTodo.classList.add('button');
+    checkTodo.value = 'Done';
+    checkTodo.type = 'button';
+    todoItem.appendChild(checkTodo);
+    checkTodo.addEventListener('click', handleCheckTodo);
+
     return todoItem;
 };
 
 const handleDeleteAllClick = function (event) {
     const todoList = document.querySelector('#todo-list');
     todoList.innerHTML = '';
+};
+
+const handleCheckTodo = function (event) {
+ const todo = event.target.parentNode.children[0];
+ if(todo.style.textDecoration === 'line-through') {
+    todo.style.textDecoration = '';
+    event.target.value = 'Done';
+ }else {
+    todo.style.textDecoration = 'line-through';
+    event.target.value = 'Undo';
+
+ }
 };
